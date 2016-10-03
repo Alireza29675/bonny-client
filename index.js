6dc6fe491d41aa5458f6cde13add3a7eabc1eb4a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2016-10-03 13:07:48 +0330

    bonny-cli-renderer make cli things easier 😊

--- index.js
+++ index.js
@@ -1,54 +1,15 @@
 const child_process = require('child_process');
 const fs = require('fs');
-const RenderKid = require('renderkid');
+const cli = require('bonny-cli-renderer');
 const config = require('./bonny.config').parse();
 
-const r = new RenderKid();
 const exec = child_process.exec;
 const spawn = child_process.spawn;
-
-r.style({
-  "title": {
-    display: "block",
-    margin: "1 0 1"
-  },
-
-  "highlight": {
-    marginRight: "1",
-    marginLeft: "1",
-    color: "bright-yellow"
-  },
-
-  "error": {
-    display: "block",
-    color: "black",
-    background: "red",
-    bullet: '"  ❌ "'
-  },
-
-  "message": {
-    display: "block",
-    color: "bright-cyan",
-    bullet: '"  👉 "',
-    margin: "0 3 1"
-  },
-
-  "success": {
-    display: "block",
-    color: "bright-green",
-    bullet: '"  ✅ "',
-    margin: "0 3 1"
-  }
-});
 
 class BonnyClient {
 
   constructor() {
-    console.log(r.render(`
-      <title>
-        🏁 The <highlight>Bonny Client Generator</highlight> is starting...
-      </title>
-    `));
+    cli.log(`<title>🏁 The <highlight>Bonny Client Generator</highlight> is starting...</title>`);
     this.webpack();
     this.gulp();
   }
@@ -57,16 +18,16 @@ class BonnyClient {
 
     const webpack = spawn('webpack');
 
-    console.log(r.render(`<message>Webpack creates ${config.bundles.items.length} bundles in '${config.bundles.fullBuildPath}'</message>`))
+    cli.log(`<message>Webpack creates ${config.bundles.items.length} bundles in '${config.bundles.fullBuildPath}'</message>`);
 
 /*
     webpack.stdout.on('data', (data) => {
-      console.log(r.render(`<message>WEBPACK: ${data}</message>`));
+      cli.log(`<message>WEBPACK: ${data}</message>`);
     });
 */
 
     webpack.stderr.on('data', (data) => {
-      console.log(r.render(`<error> WEBPACK: ${data} </error>`));
+      cli.log(`<error> WEBPACK: ${data} </error>`);
     });
   }
 
@@ -76,17 +37,17 @@ class BonnyClient {
 
     const gulp = spawn('gulp');
 
-    console.log(r.render(`<message>Gulp creates several views in '${config.views.fullBuildPath}'</message>`))
+    cli.log(`<message>Gulp creates several views in '${config.views.fullBuildPath}'</message>`);
 
 /*
     gulp.stdout.on('data', (data) => {
-      console.log(r.render(`<message>GULP: ${data}</message>`));
+      cli.log(`<message>GULP: ${data}</message>`);
     });
 */
 
     gulp.stderr.on('data', (data) => {
-      console.log(r.render(`<error> GULP: ${data} </error>`));
-      console.log(r.render(`<message>gulp will restart 5seconds later automatically...</message>`))
+      cli.log(`<error> GULP: ${data} </error>`);
+      cli.log(`<message>gulp will restart 5seconds later automatically...</message>`);
       setTimeout(()=>{
         that.gulp();
       }, 5000);
